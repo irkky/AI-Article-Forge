25e5f384bfd61166757fbd760542e9c7aed55454
Rishabh Kumar Kannaujiya <rishabhkrkannaujiya@gmail.com>
2025-11-13 19:23:16 +0530

    updated vercel handler- Remove vercel response and req

--- server/vercel-handler.ts
+++ server/vercel-handler.ts
@@ -1,9 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createApp } from './app';
 
 const appPromise = createApp();
 
-export default async (req: VercelRequest, res: VercelResponse) => {
+export default async (req: unknown, res: unknown) => {
   const app = await appPromise;
-  app(req, res);
+  app(req as any, res as any);
 };
