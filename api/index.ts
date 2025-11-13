8a4e47290d8e297ab1365ed51845cdcda53be3fe
Rishabh Kumar Kannaujiya <rishabhkrkannaujiya@gmail.com>
2025-11-13 20:25:14 +0530

    Fixed: module-missing errors

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,2 @@
-import { createApp } from "../server/app";
-
-const appPromise = createApp({ attachStatic: true });
-
-export default async function handler(req: unknown, res: unknown) {
-  const app = await appPromise;
-  app(req as any, res as any);
-}
 export { default, config } from "../server/vercel";
 
