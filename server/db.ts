7aaf4c530155a0be21272b1de8d91c658aa5dbaa
Rishabh Kumar Kannaujiya <rishabhkrkannaujiya@gmail.com>
2025-11-14 18:43:21 +0530

    import path in server/db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { createPool } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
-import * as schema from "@shared/schema.js";
+import * as schema from "../shared/dist/schema.js";
 
 const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL;
 if (!connectionString) {
